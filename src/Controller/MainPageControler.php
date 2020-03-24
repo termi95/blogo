85f97c2f3d900a7bdcfe303e390683f6c346034d
Andrzej Sokolowski <sokolowski.a95@gmail.com>
2020-03-24 22:12:09 +0100

    initial commit

---
+++ src/Controller/MainPageControler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainPageControler extends AbstractController 
+{
+    
+    /**
+     * Undocumented function
+     *
+     * @return Response
+     */
+    public function index():Response
+    {
+
+        return $this->render('mainPage.html.twig');
+    }
+}
